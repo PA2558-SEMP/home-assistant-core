a3cc78bba169335e3e9c41be1c214eb9aa30cbf8
jagadish <jagadish.vaddiparthi.1296@gmail.com>
2024-10-06 20:12:33 +0200

    Merged if statements

--- homeassistant/components/knx/select.py
+++ homeassistant/components/knx/select.py
@@ -82,14 +82,12 @@ class KNXSelect(KnxYamlEntity, SelectEntity, RestoreEntity):
     async def async_added_to_hass(self) -> None:
         """Restore last state."""
         await super().async_added_to_hass()
-        if not self._device.remote_value.readable and (
-            last_state := await self.async_get_last_state()
-        ):
-            if (
-                last_state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE)
-                and (option := self._option_payloads.get(last_state.state)) is not None
-            ):
-                self._device.remote_value.update_value(option)
+        if not self._device.remote_value.readable:
+            last_state = await self.async_get_last_state()
+            if last_state and last_state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE):
+                option = self._option_payloads.get(last_state.state)
+                if option is not None:
+                    self._device.remote_value.update_value(option)
 
     def after_update_callback(self, device: XknxDevice) -> None:
         """Call after device was updated."""

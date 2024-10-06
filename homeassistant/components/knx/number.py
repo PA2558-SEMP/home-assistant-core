38886093d1cd0aa43ba788e7ad78ff6a3bf2d6cd
jagadish <jagadish.vaddiparthi.1296@gmail.com>
2024-10-06 19:33:47 +0200

    merging if statements

--- homeassistant/components/knx/number.py
+++ homeassistant/components/knx/number.py
@@ -91,11 +91,11 @@ class KNXNumber(KnxYamlEntity, RestoreNumber):
         """Restore last state."""
         await super().async_added_to_hass()
         if (
-            not self._device.sensor_value.readable
-            and (last_state := await self.async_get_last_state())
-            and (last_number_data := await self.async_get_last_number_data())
-        ):
-            if last_state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE):
+        not self._device.sensor_value.readable
+        and (last_state := await self.async_get_last_state())
+        and (last_number_data := await self.async_get_last_number_data())
+        and last_state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE)
+    ):
                 self._device.sensor_value.value = last_number_data.native_value
 
     @property

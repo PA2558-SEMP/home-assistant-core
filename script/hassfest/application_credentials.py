9e9496fea9fdb37e6a2e841ed93b669bdc2b597f
DembaSE <demba@DembaLegionT5.>
2024-11-24 13:40:23 +0100

    Mark integrations as unused in generate function to maintain clarity

--- script/hassfest/application_credentials.py
+++ script/hassfest/application_credentials.py
@@ -42,8 +42,9 @@ def validate(integrations: dict[str, Integration], config: Config) -> None:
         )
 
 
-def generate(config: Config) -> None:
+def generate(integrations: dict[str, Integration], config: Config) -> None:
     """Generate application_credentials data."""
+    _ = integrations
     application_credentials_path = (
         config.root / "homeassistant/generated/application_credentials.py"
     )

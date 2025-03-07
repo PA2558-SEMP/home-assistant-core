554dd0e5d059b557ac083524f06c1b408f663494
rahulreddy1911 <rahulputta2001@gmail.com>
2025-03-07 23:53:07 +0100

    Addressed the issue

--- homeassistant/components/lidarr/__init__.py
+++ homeassistant/components/lidarr/__init__.py
@@ -53,38 +53,39 @@ async def async_setup_entry(hass: HomeAssistant, entry: LidarrConfigEntry) -> bo
         session=async_get_clientsession(hass, host_configuration.verify_ssl),
         request_timeout=60,
     )
+    
     class LidarrData:
-    def __init__(self, disk_space, queue, status, wanted):
-        self.disk_space = disk_space
-        self.queue = queue
-        self.status = status
-        self.wanted = wanted
+        def __init__(self, disk_space, queue, status, wanted):
+            self.disk_space = disk_space
+            self.queue = queue
+            self.status = status
+            self.wanted = wanted
 
+    data = LidarrData(
+        disk_space=DiskSpaceDataUpdateCoordinator(hass, host_configuration, lidarr),
+        queue=QueueDataUpdateCoordinator(hass, host_configuration, lidarr),
+        status=StatusDataUpdateCoordinator(hass, host_configuration, lidarr),
+        wanted=WantedDataUpdateCoordinator(hass, host_configuration, lidarr),
+    )
 
-data = LidarrData(
-    disk_space=DiskSpaceDataUpdateCoordinator(hass, host_configuration, lidarr),
-    queue=QueueDataUpdateCoordinator(hass, host_configuration, lidarr),
-    status=StatusDataUpdateCoordinator(hass, host_configuration, lidarr),
-    wanted=WantedDataUpdateCoordinator(hass, host_configuration, lidarr),
-)
+    for field_name in vars(data):
+        coordinator = getattr(data, field_name)
+        await coordinator.async_config_entry_first_refresh()
 
-for field_name in vars(data):
-    coordinator = getattr(data, field_name)
-    await coordinator.async_config_entry_first_refresh()
+    device_registry = dr.async_get(hass)
+    device_registry.async_get_or_create(
+        config_entry_id=entry.entry_id,
+        configuration_url=entry.data[CONF_URL],
+        entry_type=DeviceEntryType.SERVICE,
+        identifiers={(DOMAIN, entry.entry_id)},
+        manufacturer=DEFAULT_NAME,
+        sw_version=data.status.data,
+    )
 
-device_registry = dr.async_get(hass)
-device_registry.async_get_or_create(
-    config_entry_id=entry.entry_id,
-    configuration_url=entry.data[CONF_URL],
-    entry_type=DeviceEntryType.SERVICE,
-    identifiers={(DOMAIN, entry.entry_id)},
-    manufacturer=DEFAULT_NAME,
-    sw_version=data.status.data,
-)
+    entry.runtime_data = data
 
-entry.runtime_data = data
-
-await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
 
 
 
@@ -111,3 +112,4 @@ class LidarrEntity(CoordinatorEntity[LidarrDataUpdateCoordinator[T]]):
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, coordinator.config_entry.entry_id)}
         )
+
